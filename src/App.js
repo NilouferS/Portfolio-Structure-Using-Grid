a56ed0abb04f7c121ae33b7e24861a566bfaf832
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-28 11:20:07 +0100

    portfolio

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Skills from './Skills';
 import Project from './Project';
 import Contact from './Contact';
 import Header from './Header';
+import self from './images/self.jpg'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className='container'>
+        <img id='image' src={self} alt={self}></img>
         <Header></Header>
         <About></About>
         <Skills></Skills>
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import './header.css';
 
 export default function Header() {
     return (
         <div className='header'>
-            <div>
+            <div className='headerItem'>
                 <h3>Niloufer.Anis.Shaikh</h3>
                 <h4>Full Stack Developer</h4>
             </div>
-            <div className='sidebar'>
-                <ul>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Project</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
+            <ul className='headerItem'>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Project</li>
+                <li>Contact</li>
+            </ul>
+           
         </div>
     );
 }
