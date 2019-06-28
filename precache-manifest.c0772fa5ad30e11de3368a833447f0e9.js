2cbac7375857a9316f6186703e6a865ff5af9621
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-28 13:24:05 +0100

    Updates

---
+++ precache-manifest.c0772fa5ad30e11de3368a833447f0e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a026f41d1b27f4fe5e046fe4ecb79a0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ffc71ce9113a679867d9",
+    "url": "/portfolio/static/css/main.210dd15b.chunk.css"
+  },
+  {
+    "revision": "3cd2d4d6116699026de3",
+    "url": "/portfolio/static/js/2.f34114c2.chunk.js"
+  },
+  {
+    "revision": "ffc71ce9113a679867d9",
+    "url": "/portfolio/static/js/main.4a38264e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/portfolio/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/portfolio/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "e895e23905faaa074aac85f50d5017a1",
+    "url": "/portfolio/static/media/self.e895e239.jpg"
+  },
+  {
+    "revision": "6ded14c8a9e078f1d9c305186271078d",
+    "url": "/portfolio/static/media/wordpress.6ded14c8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio1/precache-manifest.2e5efd349db9e70e8625d73220f5381a.js"
+  "/portfolio/precache-manifest.c0772fa5ad30e11de3368a833447f0e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
